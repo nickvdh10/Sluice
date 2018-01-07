e137a07966466e850c6f79b6e2784a02720815a5
Nick van den Heuvel <nick.vandenheuvel@student.fontys.nl>
2018-01-07 22:40:11 +0100

    Door test

--- test/DoorTest.cpp
+++ test/DoorTest.cpp
@@ -6,13 +6,31 @@ class DoorTest : public ::testing::Test
 	 protected:
 		DoorTest() 
 		{
-		  testPtr = new Door ("Right");
+		  testPtr = new Door (NULL,"Right");
 		}
 		Door* testPtr;
 };
 	 
-TEST_F(DoorTest)
+TEST_F(DoorTest, doorParamsTest)
 {
     EXPECT_EQ(false,testPtr->GetDoorStatus());
 	EXPECT_EQ("Right",testPtr->GetDoorSide());
+	testPtr->SetDoorStatus(true);
+	EXPECT_EQ(true,testPtr->GetDoorStatus());
+}
+
+TEST_F(DoorTest, hasValvesTest)
+{
+	std::vector<Valve*> v = testPtr->GetValves();
+	EXPECT_EQ(3, v.size());
+}
+
+TEST_F(DoorTest, createMessageTest)
+{
+	std::string compareStr1 = "GetDoorRight;";
+	std::string compareStr2 = "SetDoorRight:open;";
+	std::string compareStr3 = "SetDoorRight:close;";
+	EXPECT_EQ(compareStr1, testPtr->CreateDoorMessage("", true);
+	EXPECT_EQ(compareStr2, testPtr->CreateDoorMessage("open", false);
+	EXPECT_EQ(compareStr3, testPtr->CreateDoorMessage("close", false);
 }
